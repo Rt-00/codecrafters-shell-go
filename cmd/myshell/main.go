fd3939b50cfda0a5ddf08244eda4fc4b0ae9cc2f
Rafael Toledo <toledorafael304@gmail.com>
2024-06-15 16:15:34 -0300

    pass 4st stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,11 +7,40 @@ import (
 	"strings"
 )
 
+var availableCommands = map[string]func([]string){
+	"exit": exit,
+}
+
+func exit(args []string) {
+	if strings.Join(args, "") != "0" {
+		os.Exit(1)
+	} else {
+		os.Exit(0)
+	}
+}
+
+func evaluateInput(input string) {
+	inputSplitted := strings.Split(input, " ")
+
+	command, args := inputSplitted[0], inputSplitted[1:]
+
+	functionFounded, ok := availableCommands[command]
+
+	if ok {
+		functionFounded(args)
+	} else {
+		fmt.Printf("%s: command not found\n", strings.TrimRight(input, "\n"))
+	}
+}
+
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		fmt.Printf("%s: command not found\n", strings.TrimRight(input, "\n"))
+		input = strings.TrimRight(input, "\n")
+
+		evaluateInput(input)
+
 	}
 }
